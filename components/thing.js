e6bb3502dfced9f6fd9fe1f664371200af725a3c
Ben Johnson <thisguyistotallyben@gmail.com>
2022-10-10 22:36:44 -0400

    a

--- components/thing.js
+++ components/thing.js
@@ -2,7 +2,6 @@ import BwcElement from "../utils/BwcElement.js";
 
 export default class Thing extends BwcElement {
     constructor() {
-        super(this);
-        this.innerHTML = '<div>HELLO WORLD</div>';
+        super('thing');
     }
 }
